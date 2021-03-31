d7fadc2e84bd7e4a6ad12d47ac823633d604459f
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-31 21:07:22 +1300

    Arrays in TypeScript

--- types.js
+++ types.js
@@ -1,9 +1,21 @@
+// TypeScript Types
 var myString;
 var myNum;
 var myBool;
 var myVar;
+// let strArr: string[];
+// let numArr: number[];
+// let boolArr: boolean[];
+var strArr;
+var numArr;
+var boolArr;
+var strNumTuple;
 myString = 'Hello'.slice(0, 3);
 myNum = 3 * 3;
 myBool = false;
 myVar = true;
-console.log(myVar);
+strArr = ['Hello', 'World'];
+numArr = [1, 2, 3, 4, 5,];
+boolArr = [true, false, false, true];
+strNumTuple = ['Hi', 7, false];
+console.log(strNumTuple);
--- types.ts
+++ types.ts
@@ -1,11 +1,28 @@
+// TypeScript Types
 let myString: string;
 let myNum: number;
 let myBool: boolean;
 let myVar: any;
+
+// let strArr: string[];
+// let numArr: number[];
+// let boolArr: boolean[];
+
+let strArr: Array<string>;
+let numArr: Array<number>;
+let boolArr: Array<boolean>;
+
+let strNumTuple: [string, number, any];
 
 myString = 'Hello'.slice(0,3);
 myNum = 3 * 3;
 myBool = false;
 myVar = true;
 
-console.log(myVar);
+strArr = ['Hello','World'];
+numArr = [1,2,3,4,5,];
+boolArr = [true, false, false, true];
+
+strNumTuple = ['Hi', 7, false];
+
+console.log(strNumTuple);
