e9b53c7c4bdc918c3d1da32b476d915ba63fd2f2
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-31 20:31:33 +1300

    types: Any

--- types.js
+++ types.js
@@ -1,7 +1,9 @@
 var myString;
 var myNum;
 var myBool;
+var myVar;
 myString = 'Hello'.slice(0, 3);
 myNum = 3 * 3;
 myBool = false;
-console.log(myString);
+myVar = true;
+console.log(myVar);
--- types.ts
+++ types.ts
@@ -1,9 +1,11 @@
 let myString: string;
 let myNum: number;
 let myBool: boolean;
+let myVar: any;
 
 myString = 'Hello'.slice(0,3);
 myNum = 3 * 3;
 myBool = false;
+myVar = true;
 
-console.log(myString);
+console.log(myVar);
