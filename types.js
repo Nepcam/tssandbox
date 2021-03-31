1973a93bf9e1e4df8de8d3ea0afa945887790b9d
Cameron NEPE <cn93@students.waikato.ac.nz>
2021-03-31 20:28:57 +1300

    type tutu

---
+++ types.js
@@ -0,0 +1,7 @@
+var myString;
+var myNum;
+var myBool;
+myString = 'Hello' + ' ' + ' World';
+myNum = 3 * 3;
+myBool = false;
+console.log(myString);
---
+++ types.ts
@@ -0,0 +1,9 @@
+let myString: string;
+let myNum: number;
+let myBool: boolean;
+
+myString = 'Hello'+ ' '+' World';
+myNum = 3 * 3;
+myBool = false;
+
+console.log(myString);
